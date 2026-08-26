3b69145eb5e4f5177ba3d22bd9eb4cae62c93dd8
Liu Rui <lovearuis@gmail.com>
2026-08-26 15:31:18 +0800

    Initial Commit

---
+++ BatonTests/BatonTests.swift
@@ -0,0 +1,19 @@
+//
+//  BatonTests.swift
+//  BatonTests
+//
+//  Created by 牧云踏歌 on 2026/8/26.
+//
+
+import Testing
+@testable import Baton
+
+struct BatonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
